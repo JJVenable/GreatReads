fbb48f215b724930fa55e82fa05ebb5aeb9892c4
Lieba Pil <pil.lieba@gmail.com>
2022-01-14 10:38:52 -0800

    finished user seed

--- seeders/20220114182952-user.js
+++ seeders/20220114182952-user.js
@@ -2,23 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    
 
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          password: '12345678',
+          username:'jdoe',
+          isAdmin: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],{}
+    )
+  },
+  
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('users', null, {})
   }
 };
