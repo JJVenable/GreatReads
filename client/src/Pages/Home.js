7c6cf1e37b89649d7fc6c56490a14198cdb07efb
andora814 <smith.doran@gmail.com>
2022-01-14 16:10:46 -0500

    initial product card service, type, action and reducer set up

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,7 @@
 // import './App.css';
-// import { Route, Switch } from 'react-router-dom'
-// import Nav from './components/Nav.js'
-// import NewSaleCard from './components/NewSaleCard'
-// import ProductCard from './components/ProductCard'
+import NewSaleCard from '../components/NewSaleCard';
+import ProductCard from '../components/ProductCard';
 // import ProductLine from './components/ProductLine'
-// import About from './pages/About'
 import { connect } from 'react-redux';
 import React, { useEffect } from 'react';
 
@@ -12,6 +9,8 @@ function Home() {
   return (
     <div className="home-page">
       <p>This is the homepage, yo!</p>
+      <NewSaleCard />
+      <ProductCard />
     </div>
   );
 }
