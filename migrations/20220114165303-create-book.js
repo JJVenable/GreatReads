51219acb797a606f0c58f6e08ccfb269731ef35d
gmenke54 <menke.gw@gmail.com>
2022-01-14 09:27:26 -0800

    updated the book model

--- migrations/20220114165303-create-book.js
+++ migrations/20220114165303-create-book.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Books', {
+    await queryInterface.createTable('books', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Books');
+    await queryInterface.dropTable('books');
   }
 };
--- models/book.js
+++ models/book.js
@@ -9,6 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Book.belongsToMany(models.Sale, {
+        through: models.CheckoutList,
+        as: 'books',
+        foreignKey: 'bookId'
+      });
     }
   }
   Book.init(
