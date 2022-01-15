78a51d84d70b5fd27e699a81ed7015f3c20f78c6
Joseph Venable <JJVenable@gmail.com>
2022-01-15 16:43:00 -0500

    buyProduct/ decrement product inventory added to ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import '../styling/ProductCard.css'
 import React, { useEffect } from 'react';
-import { RemoveProduct } from '../store/actions/ProductCardActions';
+import { RemoveProduct, UpdateProduct } from '../store/actions/ProductCardActions';
 
 const mapStateToProps = ({ productCardState }) => {
   return { productCardState };
@@ -9,17 +9,23 @@ const mapStateToProps = ({ productCardState }) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    deleteProduct: (id) => dispatch(RemoveProduct(id))
+    deleteProduct: (id) => dispatch(RemoveProduct(id)),
+    updateProduct: (id, body) => dispatch(UpdateProduct(id, body))
   };
 };
 
 function ProductCard(props) {
   let price = (props.product.price * .01).toFixed(2)
-
-  const buyProduct = () => {
+/// buy product/ reduce inventory
+  const buyProduct = (id) => {
     console.log('added to sale!')
+    console.log(props.product.inventory)
+    const newInventoryCount = props.product.inventory - 1 
+    const newBody = {"inventory" : newInventoryCount}
+    props.updateProduct(id, newBody)
   }
 
+//// delete Product 
   const delProduct = (id) => {
     console.log('delete button clicked');
     props.deleteProduct(id)
@@ -33,8 +39,9 @@ function ProductCard(props) {
       <img className='card-rating' src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.libbyhellmann.com%2Fwp-content%2Fuploads%2F2015%2F11%2F5stars.png&f=1&nofb=1" />
       <div className='buy-line'>
         <div className='card-quantity'>Only {props.product.inventory} left in stock.</div>
-        <button onClick={buyProduct}>Buy Now!</button>
+        <button onClick={()=>buyProduct(props.product.id)}>Buy Now!</button>
         <button onClick={()=>delProduct(props.product.id)}>Remove Product</button>
+
       </div>
     </div>
   );
