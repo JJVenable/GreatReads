500954b6cb8f85685ae620de9f7d6cbbae6f76e9
Joseph Venable <JJVenable@gmail.com>
2022-01-15 16:12:26 -0500

    finished update book in productCardService.js

--- client/src/services/ProductCardService.js
+++ client/src/services/ProductCardService.js
@@ -1,5 +1,6 @@
 import client from './';
 
+/// get all books
 export const GetProducts = async () => {
   try {
     const res = await client.get('/books/all');
@@ -10,6 +11,7 @@ export const GetProducts = async () => {
   }
 };
 
+/// delete book by id
 export const DeleteProductAction = async (id) => {
   try {
     await client.delete(`/books/delete/${id}`);
@@ -18,9 +20,12 @@ export const DeleteProductAction = async (id) => {
   }
 };
 
+/// update book
 export const UpdateProductService = async (id,body) => {
   try {
-    await client.put(`/books/update/${id}`);
+    const res = await client.put(`/books/update/${id}`, body);
+    console.log(res.data)
+    return res.data
   } catch (error) {
     throw error;
   }
