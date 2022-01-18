45e6780ad8439c78e02b933d68a90bada737a2c2
Joseph Venable <JJVenable@gmail.com>
2022-01-18 15:58:34 -0500

    added icons/ changed styling on icons

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,26 +1,42 @@
-import { connect } from 'react-redux';
-import React, { useEffect } from 'react';
-
+// import { connect } from 'react-redux';
+import React from 'react';
+import '../styling/Icons.css';
 function About() {
   return (
     <div className="about-page">
       <h2>Meet the Devs</h2>
       <h3>Grant Menke</h3>
+      <i class="fab fa-github"></i>
       <a href="https://github.com/gmenke54">GitHub</a>
+      <i class="fab fa-github"></i>
       <div></div>
+      <i class="fab fa-linkedin"></i>
       <a href="https://www.linkedin.com/in/grantmenke/">LinkedIn</a>
+      <i class="fab fa-linkedin"></i>
       <h3>Lieba Pil</h3>
+      <i class="fab fa-github"></i>
       <a href="https://github.com/liebapil">GitHub</a>
+      <i class="fab fa-github"></i>
       <div></div>
+      <i class="fab fa-linkedin"></i>
       <a href="https://www.linkedin.com/in/lieba-pil/">LinkedIn</a>
+      <i class="fab fa-linkedin"></i>
       <h3>Doran Smith</h3>
+      <i class="fab fa-github"></i>
       <a href="https://github.com/andora814">GitHub</a>
+      <i class="fab fa-github"></i>
       <div></div>
+      <i class="fab fa-linkedin"></i>
       <a href="https://www.linkedin.com/in/dorancsmith/">LinkedIn</a>
+      <i class="fab fa-linkedin"></i>
       <h3>Joseph Venable</h3>
+      <i class="fab fa-github"></i>
       <a href="https://github.com/JJVenable">GitHub</a>
+      <i class="fab fa-github"></i>
       <div></div>
+      <i class="fab fa-linkedin"></i>
       <a href="https://www.linkedin.com/in/jjvenable">LinkedIn</a>
+      <i class="fab fa-linkedin"></i>
     </div>
   );
 }
