9ee64068ef1ecf036f201b9c6b1127acc89c3d03
Joseph Venable <JJVenable@gmail.com>
2022-01-19 14:52:05 -0500

    moved Signed in to right

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,42 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import "../styling/NavBar.css";
-import logo from '../images/greatreadsLogo.png'
+import logo from "../images/greatreadsLogo.png";
 
 const Nav = (props) => {
-
   return (
     <header className="nav">
       <nav>
         <div>
           <Link to="/feed" className="upperNav">
-            <img src ={logo} className="app-logo" />
+            <img src={logo} className="app-logo" />
           </Link>
+
           <Link to="/about" className="upperNav about-link">
             About
           </Link>
 
-          <div>
-          {props.authenticated === true ? (
-            <div>Signed-in as: {props.user.email}</div>
-          ) : (
-            <Link to="/signin" className="upperNav about-link">
-            Sign In
-          </Link>
-          )}
-          </div>
-          <Link to="/" className="upperNav about-link" onClick={() => props.handleLogOut()}>
+          <Link
+            to="/"
+            className="upperNav about-link"
+            onClick={() => props.handleLogOut()}
+          >
             Log Out
           </Link>
           {props.authenticated === true ? (
-              <Link to="/lists" className="upperNav about-link">
-                My Lists
-              </Link>
-          ) : (
-            null
-          )}
-        </div>
+            <Link to="/lists" className="upperNav about-link">
+              My Lists
+            </Link>
+          ) : null}
 
+          <div>
+            {props.authenticated === true ? (
+              <div className="logged-in">Signed-in as: {props.user.email}</div>
+            ) : (
+              <Link to="/signin" className="upperNav about-link">
+                Sign In
+              </Link>
+            )}
+          </div>
+        </div>
       </nav>
     </header>
   );
