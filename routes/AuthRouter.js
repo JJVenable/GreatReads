9a1334b40d2f82dd419eacd8913e548744529a9f
gmenke54 <menke.gw@gmail.com>
2022-01-19 09:24:27 -0800

    debugged user auth

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/AuthController');
 
 router.post('/login', controller.Login);
 router.post('/register', controller.Register);
+router.get('/session', controller.CheckSession);
 // router.post('/password', controller.UpdatePassword);
 
 module.exports = router;
