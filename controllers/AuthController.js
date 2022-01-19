fd0a54cee7e532fc0e67b9ad016ebfd5208011d1
gmenke54 <menke.gw@gmail.com>
2022-01-18 17:33:20 -0800

    set up authentication register/login functionality on the backend

---
+++ controllers/AuthController.js
@@ -0,0 +1,63 @@
+const { User } = require('../models');
+const middleware = require('../middleware');
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true
+    });
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      };
+      let token = middleware.createToken(payload);
+      return res.send({ user: payload, token });
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const Register = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    let passwordDigest = await middleware.hashPassword(password);
+    const user = await User.create({ email, passwordDigest, name });
+    res.send(user);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// const UpdatePassword = async (req, res) => {
+//   try {
+//     const user = await User.findOne({ where: { email: req.body.email } });
+//     if (
+//       user &&
+//       (await middleware.comparePassword(
+//         user.dataValues.passwordDigest,
+//         req.body.oldPassword
+//       ))
+//     ) {
+//       let passwordDigest = await middleware.hashPassword(req.body.newPassword);
+
+//       await user.update({ passwordDigest });
+//       return res.send({ status: 'Success', msg: 'Password Updated' });
+//     }
+//     res.status(401).send({ status: 'Error', msg: 'Invalid Credentials' });
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+module.exports = {
+  Login,
+  Register
+  // UpdatePassword
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,53 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
 const APP_SECRET = process.env.APP_SECRET;
+
+const hashPassword = async (password) => {
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  return hashedPassword;
+};
+
+const comparePassword = async (storedPassword, password) => {
+  let passwordMatch = await bcrypt.compare(password, storedPassword);
+  return passwordMatch;
+};
+
+const createToken = (payload) => {
+  let token = jwt.sign(payload, APP_SECRET);
+  return token;
+};
+
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals;
+  try {
+    let payload = jwt.verify(token, APP_SECRET);
+    if (payload) {
+      return next();
+    }
+    res.status(401).send({ status: 'Error', msg: 'line 30' });
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: error });
+  }
+};
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1];
+    if (token) {
+      res.locals.token = token;
+      return next();
+    }
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'line 44' });
+  }
+};
+
+module.exports = {
+  stripToken,
+  verifyToken,
+  createToken,
+  comparePassword,
+  hashPassword
+};
