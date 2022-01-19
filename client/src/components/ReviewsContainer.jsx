c4cc707142b70437867bf153cccba7f0002809fc
andora814 <smith.doran@gmail.com>
2022-01-18 22:00:05 -0500

    added console log

--- client/src/components/ReviewsContainer.jsx
+++ client/src/components/ReviewsContainer.jsx
@@ -3,6 +3,7 @@ import ReviewCard from './ReviewCard'
 
 export default function ReviewsContainer(props) {
   console.log(props.reviews.reviews.Reviews)
+  console.log('i hope this works')
   return (
     <div> 
   {props.reviews.reviews.Reviews.map((review) => ( 
