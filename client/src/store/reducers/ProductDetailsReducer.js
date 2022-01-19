ece545b038e0df695aa7d021b490b1aa280101cf
Lieba Pil <pil.lieba@gmail.com>
2022-01-19 10:50:34 -0800

    trying new branch

--- client/src/store/reducers/ProductDetailsReducer.js
+++ client/src/store/reducers/ProductDetailsReducer.js
@@ -21,27 +21,29 @@ const iState = {
 const ProductDetailsReducer = (state = iState, action) => {
   switch (action.type) {
     case GET_PRODUCT_DETAILS:
-      // console.log(action.payload);
       return { ...state, details: action.payload };
     case GET_BOOK_WITH_ALL_REVIEWS:
-      // console.log(action.payload);
       return { ...state, reviews: { reviews: action.payload } };
     case CREATE_REVIEW:
       console.log(action.payload);
-      // let newReviewList = state.reviews.reviews.Reviews;
-      // console.log(newReviewList);
-      // newReviewList.push(action.payload);
-      // console.log(newReviewList);
+      let newReviewList = state.reviews.reviews.Reviews;
+      console.log(newReviewList);
+      newReviewList.push(action.payload);
+      console.log(newReviewList);
       return {
         ...state
-        // reviews: { ...state.reviews.reviews, Reviews: newReviewList },
+        // reviews: {
+        //   ...state.reviews.reviews,
+        //   reviews: {
+        //     Reviews: [...state.reviews.reviews.Reviews, newReviewList]
+        //   }
+        // },
         // newReview: {
         //   comments: '',
         //   rating: ''
         // }
       };
     case NEW_REVIEW:
-      console.log(action.payload);
       return {
         ...state,
         newReview: action.payload
@@ -52,9 +54,3 @@ const ProductDetailsReducer = (state = iState, action) => {
 };
 
 export default ProductDetailsReducer;
-
-// case CREATE_BOOK:
-//   let newProductsList = state.products;
-//   newProductsList.push(action.payload);
-//   console.log(newProductsList);
-//   return { ...state, products: newProductsList };
